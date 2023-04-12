1ba823fd34253ee147e15df5c1c7e8cc2f9f55ad
A.chitrarasu <chitrarasumit@gmail.com>
2023-04-12 12:57:09 +0530

    first commit

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 
 
+//require
 require('dotenv').config();
+
 const express = require("express");
-const db=require("./db/connect")
+const cors=require ("cors");
 
-
+const db=require("./db/connect");
+//require routes
 const productRoutes=require("./routes/product.routes")
+
+//calling
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
+
+//routes calling
 //custom middlewar
-
 app.use(productRoutes)
+
+
+
+
+
+
 
 
 
